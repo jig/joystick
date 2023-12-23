e69cc9e606a21d2aef88ed9b17d77a322f1fb0f9
Jordi Íñigo Griera <jig@users.noreply.github.com>
2023-12-23 16:06:34 +0100

    terminal emulation removed from the example

--- joysticktest/joysticktest.go
+++ joysticktest/joysticktest.go
@@ -1,51 +1,44 @@
 // Simple program that displays the state of the specified joystick
 //
-//     go run joysticktest.go 2
+//	go run joysticktest.go 2
+//
 // displays state of joystick id 2
 package main
 
 import (
 	"fmt"
-	"github.com/nsf/termbox-go"
-	"github.com/0xcafed00d/joystick"
 	"os"
 	"strconv"
 	"time"
-)
 
-func printAt(x, y int, s string) {
-	for _, r := range s {
-		termbox.SetCell(x, y, r, termbox.ColorDefault, termbox.ColorDefault)
-		x++
-	}
-}
+	"github.com/0xcafed00d/joystick"
+)
 
 func readJoystick(js joystick.Joystick) {
 	jinfo, err := js.Read()
-
 	if err != nil {
-		printAt(1, 5, "Error: "+err.Error())
+		fmt.Println("Error: " + err.Error())
 		return
 	}
 
-	printAt(1, 5, "Buttons:")
+	fmt.Println("Buttons:")
 	for button := 0; button < js.ButtonCount(); button++ {
 		if jinfo.Buttons&(1<<uint32(button)) != 0 {
-			printAt(10+button, 5, "X")
+			fmt.Print("X")
 		} else {
-			printAt(10+button, 5, ".")
+			fmt.Print(".")
 		}
 	}
 
+	fmt.Println()
 	for axis := 0; axis < js.AxisCount(); axis++ {
-		printAt(1, axis+7, fmt.Sprintf("Axis %2d Value: %7d", axis, jinfo.AxisData[axis]))
+		fmt.Printf("Axis %2d Value: %7d\n", axis, jinfo.AxisData[axis])
 	}
 
 	return
 }
 
 func main() {
-
 	jsid := 0
 	if len(os.Args) > 1 {
 		i, err := strconv.Atoi(os.Args[1])
@@ -63,40 +56,10 @@ func main() {
 		return
 	}
 
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer termbox.Close()
-
-	eventQueue := make(chan termbox.Event)
-	go func() {
-		for {
-			eventQueue <- termbox.PollEvent()
-		}
-	}()
-
 	ticker := time.NewTicker(time.Millisecond * 40)
 
-	for doQuit := false; !doQuit; {
-		select {
-		case ev := <-eventQueue:
-			if ev.Type == termbox.EventKey {
-				if ev.Ch == 'q' {
-					doQuit = true
-				}
-			}
-			if ev.Type == termbox.EventResize {
-				termbox.Flush()
-			}
-
-		case <-ticker.C:
-			printAt(1, 0, "-- Press 'q' to Exit --")
-			printAt(1, 1, fmt.Sprintf("Joystick Name: %s", js.Name()))
-			printAt(1, 2, fmt.Sprintf("   Axis Count: %d", js.AxisCount()))
-			printAt(1, 3, fmt.Sprintf(" Button Count: %d", js.ButtonCount()))
-			readJoystick(js)
-			termbox.Flush()
-		}
+	for range ticker.C {
+		fmt.Printf("Joystick Name: %s Axis Count: %d Button Count: %d\n", js.Name(), js.AxisCount(), js.ButtonCount())
+		readJoystick(js)
 	}
 }
